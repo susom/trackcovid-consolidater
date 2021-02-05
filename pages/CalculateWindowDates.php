7378d2b5fd9bf2903685b17f36a682ba3c14f9fd
yasukawa <LYasukawa@stanfordhealthcare.org>
2021-02-04 22:07:25 -0800

    Add support for GenPop

--- pages/CalculateWindowDates.php
+++ pages/CalculateWindowDates.php
@@ -8,7 +8,8 @@ $action = isset($_GET['action']) && !empty($_GET['action']) ? $_GET['action'] : 
 $pid = isset($_GET['pid']) && !empty($_GET['pid']) ? $_GET['pid'] : null;
 
 $chart_pid = $module->getSystemSetting('chart-pid');
-if ($pid != $chart_pid) {
+$genpop_pid = $module->getSystemSetting('genpop-pid');
+if (($pid != $chart_pid) && ($pid != $genpop_pid)) {
     $module->emError("This module should only be run on the Chart study and not pid $pid");
     print true;
     return;
@@ -25,13 +26,23 @@ if ($action == "calc") {
     $skip_records = explode(',', $list_of_skip_records);
     $module->emDebug("List of records to skip: " . json_encode($skip_records));
 
-    $baseline_event = 'baseline_arm_1';
+    if ($pid == $genpop_pid) {
+        $baseline_event = 'baseline_visit_arm_1';
+        $wk2_event_name = 'followup_visit_1_arm_1';
+        $saved_date_collected = 'date_of_visit';
+        $saved_reservation_date = 'reservation_date';
+    } else if ($pid == $chart_pid) {
+        $baseline_event = 'baseline_arm_1';
+        $wk2_event_name = 'wk2_arm_1';
+        $saved_date_collected = 'date_collected';
+        $saved_reservation_date = 'reservation_date';
+    }
     $baseline_event_id = REDCap::getEventIdFromUniqueEvent($baseline_event);
 
     // Store the record_id, birth_date and mrn in a table track_covid_mrn_dob
     $params = array(
         'return_format' => 'array',
-        'fields' => array('record_id', 'redcap_event_name', 'date_collected', 'reservation_date'),
+        'fields' => array('record_id', 'redcap_event_name', $saved_date_collected, $saved_reservation_date),
         'events' => $baseline_event
     );
     $records = REDCap::getData($params);
@@ -45,7 +56,7 @@ if ($action == "calc") {
 
     // Retrieve the records in wk2 so we can compare if the dates have changed. If not,
     // the windows do not need to be updated
-    $wk2_event_name = 'wk2_arm_1';
+    if ($pid)
     $params = array(
         'return_format' => 'array',
         'fields' => array('record_id', 'redcap_event_name', 'visit_window_lower', 'visit_window_upper'),
@@ -58,22 +69,21 @@ if ($action == "calc") {
     $lower = -$module->getProjectSetting("lower-limit");
     $upper = $module->getProjectSetting("upper-limit");
     $schedule_json = $module->getProjectSetting("window-schedule");
-    $followup_days = json_decode($schedule_json);
     $module->emDebug("Lower Limit: " . $lower . ", Upper Limit: " . $upper . ", json schedule: " . $schedule_json);
+
+    $followup_days = json_decode($schedule_json);
     foreach($followup_days as $ndays => $event) {
         $module->emDebug("Ndays: $ndays and event name: $event");
     }
 
     // Loop over all records to set visit windows
-    $save_data = '';
+    $ncnt = 0;
     foreach ($records as $record) {
-
-        $this_record = '';
 
         // Extract the baseline date for each record
         $record_id = $record[$baseline_event_id]['record_id'];
-        $date_collected = $record[$baseline_event_id]['date_collected'];
-        $appointment_date = $record[$baseline_event_id]['reservation_date'];
+        $date_collected = $record[$baseline_event_id][$saved_date_collected];
+        $appointment_date = $record[$baseline_event_id][$saved_reservation_date];
 
         // First use date_collected but if that is not set, use reservation date
         if (!empty($date_collected)) {
@@ -86,36 +96,28 @@ if ($action == "calc") {
 
         // If we found a date to use as baseline, calculate the visit windows
         if (!is_null($use_date) and (is_null($skip_records) or !in_array($record_id, $skip_records))) {
-            $this_record = calculateWindowLimits($record_id, $use_date, $wk2_dates, $wk2_event_id, $lower, $upper, $followup_days);
-
-            if (($save_data != '') and ($this_record != '')) {
-                $save_data .= ',' . $this_record;
-            } else if ($save_data == '') {
-                $save_data = $this_record;
+            $this_record = calculateWindowLimits($record_id, $use_date, $wk2_dates, $wk2_event_id, $lower, $upper, $followup_days, $wk2_event_name);
+            if (!empty($this_record)) {
+                $save_data = '[' . $this_record . ']';
+                $return = REDCap::saveData('json', $save_data);
+                //$module->emDebug("Just saved $record_id, return: " . json_encode($return));
             }
         }
-
+        $ncnt++;
+        if (($ncnt % 50) == 0) {
+            $module->emDebug("Just processed record $record_id for a total of $ncnt records");
+        }
     }
 
-    // Once we calculate windows for each event, save the data in Redcap
-    if ($save_data != '') {
-        $save_data = '[' . $save_data . ']';
-        $return = REDCap::saveData('json', $save_data);
-        $module->emDebug("Return from window date calculations saveData: " . json_encode($return));
-        $status = true;
-    } else {
-        $module->emDebug("No visit window dates to save");
-        $status = true;
-    }
+    $module->emDebug("Done saving window dates");
 
-    print $status;
+    print true;
     return;
 }
 
-function calculateWindowLimits($record_id, $baseline_date, $wk2_dates, $wk2_event_id, $lower, $upper, $followup_days) {
+function calculateWindowLimits($record_id, $baseline_date, $wk2_dates, $wk2_event_id, $lower, $upper, $followup_days, $wk2_event_name) {
 
     global $module;
-
 
     $data_to_save = '';
     foreach($followup_days as $followup => $event_name) {
@@ -133,7 +135,7 @@ function calculateWindowLimits($record_id, $baseline_date, $wk2_dates, $wk2_even
             $data_to_save .= ',';
         }
 
-        if ($this_record['redcap_event_name'] == 'wk2_arm_1') {
+        if ($this_record['redcap_event_name'] == $wk2_event_name) {
             if (($this_record['visit_window_lower'] == $wk2_dates[$record_id][$wk2_event_id]['visit_window_lower'])
                 and ($this_record['visit_window_upper'] == $wk2_dates[$record_id][$wk2_event_id]['visit_window_upper'])) {
                 break;
